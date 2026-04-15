a6e26f1843dc90087dd50e05b5a612c06e9b9f0a
kazviz <137312421+kazviz@users.noreply.github.com>
2026-04-15 23:41:51 +0700

    refactor: remove unused locales module from lib.rs

--- crates/tui/src/lib.rs
+++ crates/tui/src/lib.rs
@@ -1,11 +1,9 @@
 pub use crate::assets::*;
-pub use crate::locales::*;
 pub use crate::templates::*;
 pub use crate::theme::*;
 pub use crate::widgets::*;
 
 mod assets;
-mod locales;
 mod templates;
 mod theme;
 pub mod widgets;
